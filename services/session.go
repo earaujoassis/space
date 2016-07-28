5e31636d0360125c29ed69317959a7c548279ae0
Ewerton Assis <earaujoassis@gmail.com>
2016-07-27 22:48:36 -0300

    Check token expiration; closes #16

--- services/session.go
+++ services/session.go
@@ -1,11 +1,36 @@
 package services
 
 import (
+    "time"
+
     "github.com/earaujoassis/space/datastore"
     "github.com/earaujoassis/space/models"
 )
 
+const (
+    eternalExpirationLength    int64 = 0
+    largestExpirationLength    int64 = 3600 // 60 min
+    defaultExpirationLength    int64 = 1800 // 30 min
+    shortestExpirationLength   int64 = 300  //  5 min
+)
+
+func expirationLengthForTokenType(tokenType string) int64 {
+    switch tokenType {
+    case models.AccessToken:
+        return largestExpirationLength
+    case models.RefreshToken:
+        return eternalExpirationLength
+    case models.GrantToken:
+        return shortestExpirationLength
+    case models.ActionToken:
+        return shortestExpirationLength
+    default:
+        return defaultExpirationLength
+    }
+}
+
 func CreateSession(user models.User, client models.Client, ip, userAgent, scopes, tokenType string) models.Session {
+    expirationLength := expirationLengthForTokenType(tokenType)
     var session models.Session = models.Session{
         User: user,
         Client: client,
@@ -13,10 +38,22 @@ func CreateSession(user models.User, client models.Client, ip, userAgent, scopes
         UserAgent: userAgent,
         Scopes: scopes,
         TokenType: tokenType,
+        ExpiresIn: expirationLength,
     }
     dataStore := datastore.GetDataStoreConnection()
-    dataStore.Create(&session)
-    return session
+    result := dataStore.Create(&session)
+    if count := result.RowsAffected; count > 0 {
+        return session
+    }
+    return models.Session{}
+}
+
+func withinExpirationWindow(session models.Session) bool {
+    now := time.Now().UTC().Unix()
+    if session.ExpiresIn == eternalExpirationLength || session.Moment + session.ExpiresIn >= now {
+        return true
+    }
+    return false
 }
 
 func FindSessionByUUID(uuid string) models.Session {
@@ -29,6 +66,12 @@ func FindSessionByUUID(uuid string) models.Session {
         Preload("User.Language").
         Where("uuid = ? AND invalidated = false", uuid).
         First(&session)
+    if session.ID != 0 {
+        if !withinExpirationWindow(session) {
+            InvalidateSession(session)
+            return models.Session{}
+        }
+    }
     return session
 }
 
@@ -42,6 +85,12 @@ func FindSessionByToken(token, tokenType string) models.Session {
         Preload("User.Language").
         Where("token = ? AND token_type = ? AND invalidated = false", token, tokenType).
         First(&session)
+    if session.ID != 0 {
+        if !withinExpirationWindow(session) {
+            InvalidateSession(session)
+            return models.Session{}
+        }
+    }
     return session
 }
 
@@ -57,7 +106,9 @@ func ActiveSessionsForClient(clientKey string) int64 {
 
     dataStoreSession := datastore.GetDataStoreConnection()
     dataStoreSession.
-        Raw("SELECT count(*) AS count FROM sessions JOIN clients ON clients.id = sessions.client_id WHERE clients.key = ? AND invalidated = false AND token_type = ? OR token_type = ?;",
+        Raw("SELECT count(*) AS count " +
+            "FROM sessions JOIN clients ON clients.id = sessions.client_id " +
+            "WHERE clients.key = ? AND invalidated = false AND token_type = ? OR token_type = ?;",
             clientKey, models.AccessToken, models.RefreshToken).
         Scan(&count)
     return count.Count
