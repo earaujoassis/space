545658149426e47a94d792de954d38dafb42bde1
Ewerton Assis <earaujoassis@gmail.com>
2016-11-03 19:16:27 -0200

    Logger's email messages should be sent in production only

--- config/config.go
+++ config/config.go
@@ -3,11 +3,12 @@ package config
 import (
     "fmt"
     "os"
+    "strings"
     "github.com/spf13/viper"
 )
 
 func init() {
-    var environment = os.Getenv("ENVIRONMENT")
+    var environment = strings.ToLower(os.Getenv("ENVIRONMENT"))
     if environment == "" {
         environment = "development"
     }
@@ -19,6 +20,14 @@ func init() {
     if err != nil {
         panic(fmt.Errorf("Fatal error config file: %s \n", err))
     }
+}
+
+func Environment() string {
+    return viper.Get("environment").(string)
+}
+
+func IsEnvironment(env string) bool {
+    return strings.ToLower(env) == Environment()
 }
 
 func GetConfig(key string) interface{} {
--- services/logger/actions.go
+++ services/logger/actions.go
@@ -1,21 +1,28 @@
 package logger
 
 import (
+    "fmt"
     "time"
 
     "github.com/earaujoassis/space/services/mailer"
+    "github.com/earaujoassis/space/config"
     "github.com/earaujoassis/space/utils"
 )
 
 func LogAction(name string, data utils.H) {
-    switch name {
-    case "user.created":
-        data["Year"] = time.Now().Year()
-        message := mailer.CreateMessage("user.created.html", data)
-        mailer.SendEmail("Welcome to QuatroLabs services", message, data["Email"].(string))
-    case "session.created":
-        data["Year"] = time.Now().Year()
-        message := mailer.CreateMessage("session.created.html", data)
-        mailer.SendEmail("A new session created at QuatroLabs", message, data["Email"].(string))
+    if config.IsEnvironment("production") {
+        switch name {
+        case "user.created":
+            data["Year"] = time.Now().Year()
+            message := mailer.CreateMessage("user.created.html", data)
+            mailer.SendEmail("Welcome to QuatroLabs services", message, data["Email"].(string))
+        case "session.created":
+            data["Year"] = time.Now().Year()
+            message := mailer.CreateMessage("session.created.html", data)
+            mailer.SendEmail("A new session created at QuatroLabs", message, data["Email"].(string))
+        }
+    }
+    if config.IsEnvironment("development") {
+        fmt.Printf("[logger] Action `%s` with data `%v`\n", name, data)
     }
 }
