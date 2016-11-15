ac949a95a2540ea006fc275ee9ca0e94af48b5e6
Ewerton Assis <earaujoassis@gmail.com>
2016-11-14 22:36:35 -0200

    Fix: compress web assets in production

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -8,6 +8,7 @@ var source     = require('vinyl-source-stream');
 var buffer     = require('vinyl-buffer');
 
 var satellites = ['ganymede', 'io', 'europa', 'callisto'];
+var environment = process.env.NODE_ENV;
 
 gulp.task('styles', function () {
   return gulp
@@ -22,22 +23,35 @@ gulp.task('styles', function () {
                 'node_modules/foundation-sites/scss',
                 'node_modules/normalize.scss',
                 'core/styles'
-            ]
+            ],
+            outputStyle: environment == 'production' ?'compressed' : 'nested'
         }).on('error', sass.logError))
     .pipe(gulp.dest('./public/css/'));
 });
 
 Array.prototype.forEach.call(satellites, function(satellite) {
-    gulp.task(satellite, function() {
-        return browserify('./' + satellite + '/index.jsx')
-            .transform(babelify, {presets: ['es2015', 'react']})
-            .bundle()
-            .pipe(source('index.jsx'))
-            .pipe(buffer())
-            .pipe(rename(satellite + '.min.js'))
-            //.pipe(uglify())
-            .pipe(gulp.dest('./public/js/'));
-    });
+    if (environment == 'production') {
+        gulp.task(satellite, function() {
+            return browserify('./' + satellite + '/index.jsx')
+                .transform(babelify, {presets: ['es2015', 'react']})
+                .bundle()
+                .pipe(source('index.jsx'))
+                .pipe(buffer())
+                .pipe(rename(satellite + '.min.js'))
+                .pipe(uglify())
+                .pipe(gulp.dest('./public/js/'));
+        });
+    } else {
+        gulp.task(satellite, function() {
+            return browserify('./' + satellite + '/index.jsx')
+                .transform(babelify, {presets: ['es2015', 'react']})
+                .bundle()
+                .pipe(source('index.jsx'))
+                .pipe(buffer())
+                .pipe(rename(satellite + '.min.js'))
+                .pipe(gulp.dest('./public/js/'));
+        });
+    }
 })
 
 gulp.task('scripts', satellites);
