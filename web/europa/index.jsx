28dcf6c1d37822cdf75e729605eebebf04f9393f
Ewerton Assis <earaujoassis@gmail.com>
2016-11-15 22:09:10 -0200

    Remove AccountLog view (remove feature)

--- web/europa/index.jsx
+++ web/europa/index.jsx
@@ -8,14 +8,12 @@ import Settings from './components/Settings.jsx'
 
 import Applications from './components/Applications.jsx'
 import Profile from './components/Profile.jsx'
-import AccountLog from './components/AccountLog.jsx'
 
 const europa = (
     <Router history={browserHistory}>
         <Route path="/" component={Settings}>
             <IndexRoute component={Applications} name="Applications" />
             <Route path="profile" component={Profile} name="Profile" />
-            <Route path="log" component={AccountLog} name="Account log" />
         </Route>
     </Router>
 )
