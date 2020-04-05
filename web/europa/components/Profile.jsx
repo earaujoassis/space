9917440da4e0f81bcef85f43d30f599a3b1594ce
Carlos A <593456+earaujoassis@users.noreply.github.com>
2020-04-04 22:44:33 -0300

    feature: update user's password (request update link); closes #8

--- web/europa/components/Profile.jsx
+++ web/europa/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Row from '../../core/components/Row.jsx'
 import Columns from '../../core/components/Columns.jsx'
@@ -7,74 +7,90 @@ import { Entry } from '../../core/components/Form.jsx'
 import UserStore from '../stores/users'
 import UsersActions from '../actions/users'
 
-export default class Profile extends React.Component {
-    constructor() {
-        super()
-        this.state = {loading: true}
-        this._updateFromStore = this._updateFromStore.bind(this)
-    }
+const profile = () => {
+    const [storeState, setStoreState] = useState({isLoading: true})
+    const [updatePasswordRequested, setUpdatePasswordRequested] = useState(false)
 
-    componentDidMount() {
-        UserStore.addChangeListener(this._updateFromStore)
-        UsersActions.fetchProfile()
-    }
-
-    componentWillUnmount() {
-        UserStore.removeChangeListener(this._updateFromStore)
-    }
-
-    render() {
-        if (this.state.loading) {
-            return (
-                <Row>
-                    <Columns className="small-offset-1 small-10 end">
-                        <p className="text-center">Loading...</p>
-                    </Columns>
-                </Row>
-            )
+    useEffect(() => {
+        let updateLocalStoreState = () => {
+            if (UserStore.success()) {
+                let state = Object.assign({}, UserStore.getState().payload || {}, {isLoading: false})
+                setStoreState(state)
+            }
         }
 
-        const { user } = this.state
+        UserStore.addChangeListener(updateLocalStoreState)
+        UsersActions.fetchProfile()
 
+        return function cleanup() {
+            UserStore.removeChangeListener(updateLocalStoreState)
+        }
+    }, [])
+
+    const { user, isLoading } = storeState
+    let inputKey
+
+    if (isLoading) {
         return (
             <Row>
                 <Columns className="small-offset-1 small-10 end">
-                    <Row className="profile">
-                        <Columns className="small-12">
-                            <Entry field="Name" value={`${user.first_name} ${user.last_name}`} />
-                            <Entry field="Username" value={user.username} />
-                            <Entry field="Email" value={user.email} />
-                            <Entry field="Timezone" value={user.timezone_identifier} />
-                            {!UserStore.isCurrentUserAdmin() && UserStore.isFeatureGateActive('user.adminify') && (
-                                <Row className="profile-entry">
-                                    <Columns className="columns small-11 small-offset-1 field">
-                                        <input className="thin-input"
-                                            ref={(r) => this.inputKey = r}
-                                            type="text"
-                                            name="application_key"
-                                            placeholder="Application Key"
-                                            required />
-                                        <button className="button-anchor"
-                                            onClick={() => UsersActions.adminify(this.inputKey.value) }>
-                                            Make me an admin
-                                        </button>
-                                    </Columns>
-                                </Row>
-                            )}
-                            {UserStore.isCurrentUserAdmin() && (
-                                <Entry field="Role" value="Administrator" />
-                            )}
-                        </Columns>
-                    </Row>
+                    <p className="text-center">Loading...</p>
                 </Columns>
             </Row>
         )
     }
 
-    _updateFromStore() {
-        if (UserStore.success()) {
-            let state = Object.assign({}, UserStore.getState().payload || {}, {loading: false})
-            this.setState(state)
-        }
-    }
+    return (
+        <Row>
+            <Columns className="small-offset-1 small-10 end">
+                <Row className="profile">
+                    <Columns className="small-12">
+                        <Entry field="Name" value={`${user.first_name} ${user.last_name}`} />
+                        <Entry field="Username" value={user.username} />
+                        <Entry field="Email" value={user.email} />
+                        <Entry field="Timezone" value={user.timezone_identifier} />
+                        {!UserStore.isCurrentUserAdmin() && UserStore.isFeatureGateActive('user.adminify') && (
+                            <Row className="profile-entry">
+                                <Columns className="columns small-11 small-offset-1 field">
+                                    <input className="thin-input"
+                                        ref={(r) => inputKey = r}
+                                        type="text"
+                                        name="application_key"
+                                        placeholder="Application Key"
+                                        required />
+                                    <button className="button-anchor"
+                                        onClick={() => UsersActions.adminify(inputKey.value) }>
+                                        Make me an admin
+                                    </button>
+                                </Columns>
+                            </Row>
+                        )}
+                        {UserStore.isCurrentUserAdmin() && (
+                            <Entry field="Role" value="Administrator" />
+                        )}
+                    </Columns>
+                </Row>
+                <Row className="profile-actions">
+                    <Columns className="small-12">
+                        {updatePasswordRequested ? (
+                            <p>You should receive an e-mail message in the next few minutes.</p>
+                        ) : (
+                            <p><button
+                                onClick={(e) => {
+                                    e.preventDefault()
+                                    let formData = new FormData()
+                                    formData.append('request_type', 'password')
+                                    formData.append('holder', user.username)
+                                    UsersActions.requestUpdate(formData)
+                                    setUpdatePasswordRequested(true)
+                                }}
+                                className="button-anchor">Update password</button></p>
+                        )}
+                    </Columns>
+                </Row>
+            </Columns>
+        </Row>
+    )
 }
+
+export default profile
