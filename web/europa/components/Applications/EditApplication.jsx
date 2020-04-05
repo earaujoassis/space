47c82f8710bb7f0d6dead88d092fc23a46cf8580
Carlos A <593456+earaujoassis@users.noreply.github.com>
2020-04-04 23:52:07 -0300

    refactor: applications components from classes to functions

---
+++ web/europa/components/Applications/EditApplication.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import Row from '../../../core/components/Row.jsx'
+import Columns from '../../../core/components/Columns.jsx'
+
+const editApplication = ({ client, onSubmit, onCancel }) => {
+    return (
+        <form
+            className="form-common internal"
+            action="."
+            method="post"
+            onSubmit={(e) => {
+                e.preventDefault()
+                onSubmit(e.target)
+            }}>
+            <Row className="new-application">
+                <Columns className="small-5">
+                    <label htmlFor="canonical_uri">Canonical URI</label>
+                    <textarea type="url"
+                        id="canonical_uri"
+                        name="canonical_uri"
+                        placeholder="Canonical URI"
+                        pattern="((https?://.*)(\n)?)+"
+                        defaultValue={client.uri}
+                        required></textarea>
+                </Columns>
+                <Columns className="small-5">
+                    <label htmlFor="redirect_uri">Redirect URI</label>
+                    <textarea type="url"
+                        id="redirect_uri"
+                        name="redirect_uri"
+                        placeholder="Redirect URI"
+                        pattern="((https?://.*)(\n)?)+"
+                        defaultValue={client.redirect}
+                        required></textarea>
+                </Columns>
+                <Columns className="small-2 end">
+                    <div className="button-wrapper">
+                        <button className="button-anchor" type="submit">Save</button>
+                    </div>
+                    <div>
+                        <button onClick={() => onCancel()}
+                            className="button-anchor"
+                            type="button">Cancel</button>
+                    </div>
+                </Columns>
+            </Row>
+            <Row>
+                <Columns className="small-12">
+                    <p className="form-warning">
+                        Use one line for each redirect URI. Each redirect URI must be included as a canonical URI.
+                    </p>
+                </Columns>
+            </Row>
+        </form>
+    )
+}
+
+export default editApplication
