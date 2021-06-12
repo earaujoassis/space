6149a810bebcb7bc4e985b9374850278c8b98614
Carlos A <593456+earaujoassis@users.noreply.github.com>
2021-06-12 19:49:34 -0300

    chore: restructure-project according to golang project layout

---
+++ internal/models/users.go
@@ -0,0 +1,107 @@
+package models
+
+import (
+    "fmt"
+    "strings"
+
+    "golang.org/x/crypto/bcrypt"
+    "github.com/jinzhu/gorm"
+    "github.com/pquerna/otp"
+    "github.com/pquerna/otp/totp"
+
+    "github.com/earaujoassis/space/internal/security"
+)
+
+// User model/struct
+type User struct {
+    Model
+    UUID string                 `gorm:"not null;unique;index" validate:"omitempty,uuid4" json:"-"`
+    PublicID string             `gorm:"not null;unique;index" json:"public_id"`
+    Username string             `gorm:"not null;unique;index" validate:"required,alphanum,max=60" json:"-"`
+    FirstName string            `gorm:"not null" validate:"required,min=3,max=20" essential:"required,min=3,max=20" json:"first_name"`
+    LastName string             `gorm:"not null" validate:"required,min=3,max=20" essential:"required,min=3,max=20" json:"last_name"`
+    Email string                `gorm:"not null;unique;index" validate:"required,email" essential:"required,email" json:"email"`
+    Passphrase string           `gorm:"not null" validate:"required" essential:"required,min=10" json:"-"`
+    Active bool                 `gorm:"not null;default:false" json:"active"`
+    Admin bool                  `gorm:"not null;default:false" json:"-"`
+    Client Client               `gorm:"not null" validate:"exists" json:"-"`
+    ClientID uint               `gorm:"not null" json:"-"`
+    Language Language           `gorm:"not null" validate:"exists" json:"-"`
+    LanguageID uint             `gorm:"not null" json:"-"`
+    TimezoneIdentifier string   `gorm:"not null;default:'GMT'" json:"timezone_identifier"`
+    CodeSecret string           `gorm:"not null" validate:"required" json:"-"`
+    RecoverSecret string        `gorm:"not null" validate:"required" json:"-"`
+}
+
+// Authentic checks if a password + passcode combination is valid for a given User
+func (user *User) Authentic(password, passcode string) bool {
+    var validPasscode bool
+    validPassword := bcrypt.CompareHashAndPassword([]byte(user.Passphrase), []byte(password)) == nil
+    codeSecret, err := security.Decrypt(defaultKey(), user.CodeSecret)
+    if err != nil {
+        return false
+    }
+
+    validPasscode = totp.Validate(passcode, string(codeSecret))
+    return validPasscode && validPassword
+}
+
+// UpdatePassword updates an User's password
+func (user *User) UpdatePassword(password string) error {
+    crypted, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    if err == nil {
+        user.Passphrase = string(crypted)
+        return nil
+    }
+    return err
+}
+
+// GenerateCodeSecret generates a code secret for an user, in order to generate and validate passcodes
+func (user *User) GenerateCodeSecret() *otp.Key {
+    key, err := totp.Generate(totp.GenerateOpts{
+        Issuer:      "quatroLABS.com",
+        AccountName: user.Username,
+    })
+    codeSecret := key.Secret()
+    if cryptedCodeSecret, err := security.Encrypt(defaultKey(), []byte(codeSecret)); err == nil {
+        user.CodeSecret = string(cryptedCodeSecret)
+    } else {
+        user.CodeSecret = codeSecret
+    }
+    if err != nil {
+        return nil
+    }
+    return key
+}
+
+// GenerateRecoverSecret generates a recover secret string for an user
+func (user *User) GenerateRecoverSecret() (string, error) {
+    var secret = strings.ToUpper(fmt.Sprintf("%s-%s-%s-%s",
+        GenerateRandomString(4),
+        GenerateRandomString(4),
+        GenerateRandomString(4),
+        GenerateRandomString(4),))
+    if cryptedRecoverSecret, err := bcrypt.GenerateFromPassword([]byte(secret), bcrypt.DefaultCost); err == nil {
+        user.RecoverSecret = string(cryptedRecoverSecret)
+    } else {
+        return secret, err
+    }
+    return secret, nil
+}
+
+// BeforeSave User model/struct hook
+func (user *User) BeforeSave(scope *gorm.Scope) error {
+    return validateModel("validate", user)
+}
+
+// BeforeCreate User model/struct hook
+func (user *User) BeforeCreate(scope *gorm.Scope) error {
+    scope.SetColumn("UUID", generateUUID())
+    scope.SetColumn("PublicID", GenerateRandomString(32))
+    if cryptedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Passphrase), bcrypt.DefaultCost); err == nil {
+        scope.SetColumn("Passphrase", cryptedPassword)
+    } else {
+        return err
+    }
+    return nil
+}
---
+++ internal/tasks/application.go
@@ -0,0 +1,82 @@
+package tasks
+
+import (
+    "fmt"
+    "net/http"
+    "bufio"
+    "os"
+    "strings"
+    "github.com/gin-gonic/gin"
+
+    "github.com/earaujoassis/space/internal/api"
+    "github.com/earaujoassis/space/internal/config"
+    "github.com/earaujoassis/space/internal/datastore"
+    "github.com/earaujoassis/space/internal/feature"
+    "github.com/earaujoassis/space/internal/utils"
+    "github.com/earaujoassis/space/internal/web"
+)
+
+// Server is used to start and serve the application (REST API + Web front-end)
+func Server() {
+    datastore.Start()
+    router := gin.Default()
+    router.Use(func(c *gin.Context) {
+        defer func(c *gin.Context) {
+            // TODO It is not displaying/logging the error
+            if rec := recover(); rec != nil {
+                // var accept = c.Request.Header.Get("Accept")
+                // We're not checking Accept because there are only two paths for JSON responses: starting with /api and starting with /token
+                // We could check for X-Requested-With == 'XMLHttpRequest' as well
+                if path := c.Request.URL.Path; strings.HasPrefix(path, "/api") || strings.HasPrefix(path, "/token") {
+                    c.JSON(http.StatusInternalServerError, utils.H{
+                        "_status":  "error",
+                        "_message": "Bad server",
+                        "error": "The server found an error; aborting",
+                    })
+                } else {
+                    c.HTML(http.StatusInternalServerError, "error.internal", utils.H{
+                        "Title": " - Bad Server",
+                        "Internal": true,
+                    })
+                }
+            }
+        }(c)
+        c.Next()
+    })
+    router.NoRoute(func(c *gin.Context) {
+        // var accept = c.Request.Header.Get("Accept")
+        // We're not checking Accept because there are only two paths for JSON responses: starting with /api and starting with /token
+        // We could check for X-Requested-With == 'XMLHttpRequest' as well
+        if path := c.Request.URL.Path; strings.HasPrefix(path, "/api") || strings.HasPrefix(path, "/token") {
+            c.JSON(http.StatusNotFound, utils.H{
+                "_status":  "error",
+                "_message": "Not found",
+                "error": "Resource path not found",
+            })
+        } else {
+            c.HTML(http.StatusNotFound, "error.not_found", utils.H{
+                "Title": " - Resource Not Found",
+                "Internal": true,
+            })
+        }
+    })
+    web.ExposeRoutes(router)
+    restAPI := router.Group("/api")
+    api.ExposeRoutes(restAPI)
+    router.Run(fmt.Sprintf(":%v", config.GetEnvVar("PORT")))
+}
+
+// ToggleFeature is used to enable or disable a feature-gate
+func ToggleFeature() {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Feature key: ")
+    featureKey, _ := reader.ReadString('\n')
+    featureKey = strings.Trim(featureKey, "\n")
+    if feature.IsActive(featureKey) {
+        feature.Disable(featureKey)
+        fmt.Printf("Key `%s` is disabled\n", featureKey)
+    } else {
+        feature.Enable(featureKey)
+        fmt.Printf("Key `%s` is enabled\n", featureKey)
+    }
+}
