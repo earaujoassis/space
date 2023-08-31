72c192fd05adf3964830426286967537fb26ecf9
Carlos A <588595+earaujoassis@general.noreply.quatrolabs.com>
2023-08-31 04:04:36 -0300

    feature: check if feature flag is defined

---
+++ internal/feature/features.go
@@ -0,0 +1,17 @@
+package feature
+
+import (
+	"golang.org/x/exp/slices"
+)
+
+const (
+	// UserCreate make it possible to create users (through sign-up)
+	UserCreate   string = "user.create"
+	// UserAdminify make it possible to set users as admin (using the application key)
+	UserAdminify string = "user.adminify"
+)
+
+func IsFeatureAvailable(feature string) bool {
+	availableFeatures := []string{UserCreate, UserAdminify}
+	return slices.Contains(availableFeatures, feature)
+}
--- internal/tasks/application.go
+++ internal/tasks/application.go
@@ -1,12 +1,9 @@
 package tasks
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"os"
-	"strings"
 	"log"
 	"errors"
 	"runtime/debug"
@@ -14,7 +11,6 @@ import (
 	"github.com/earaujoassis/space/internal/api"
 	"github.com/earaujoassis/space/internal/config"
 	"github.com/earaujoassis/space/internal/datastore"
-	"github.com/earaujoassis/space/internal/feature"
 	"github.com/earaujoassis/space/internal/utils"
 	"github.com/earaujoassis/space/internal/web"
 )
@@ -63,19 +59,4 @@ func Server() {
 	restAPI := router.Group("/api")
 	api.ExposeRoutes(restAPI)
 	router.Run(fmt.Sprintf(":%v", config.GetEnvVar("PORT")))
-}
-
-// ToggleFeature is used to enable or disable a feature-gate
-func ToggleFeature() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Feature key: ")
-	featureKey, _ := reader.ReadString('\n')
-	featureKey = strings.Trim(featureKey, "\n")
-	if feature.IsActive(featureKey) {
-		feature.Disable(featureKey)
-		fmt.Printf("Key `%s` is disabled\n", featureKey)
-	} else {
-		feature.Enable(featureKey)
-		fmt.Printf("Key `%s` is enabled\n", featureKey)
-	}
 }
