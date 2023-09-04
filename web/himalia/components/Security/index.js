6b3f1535bb8b360091377add0ba672151b80ed00
Carlos A <588595+earaujoassis@general.noreply.quatrolabs.com>
2023-09-04 12:45:46 -0300

    feature: connect himalia/security to the backend

--- web/himalia/components/Security/index.js
+++ web/himalia/components/Security/index.js
@@ -1,8 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+
+import * as actions from '@actions'
 
 import './style.css'
 
-const security = () => {
+const security = ({
+    fetchUserProfile,
+    requestResetPassword,
+    requestResetSecretCodes,
+    adminifyUser,
+    loading,
+    application,
+    user }) => {
+
+    useEffect(() => {
+        fetchUserProfile(application.user_id, application.action_token)
+    }, [])
+
+    const [applicationKey, setApplicationKey] = useState('')
+    const [resetPasswordRequested, setResetPasswordRequested] = useState(false)
+    const [resetSecretCodesRequested, setResetSecretCodesRequested] = useState(false)
+
+    const handleKeypressForAdminify = (e) => {
+        if (e.key === 'Enter') {
+            adminifyUser(application.user_id, applicationKey, application.action_token)
+        }
+    }
+
+    const adminifyUserBox = application && application.user_is_admin ? null : (
+        <div className="globals__warning-box">
+            <h3>Become an admin user</h3>
+            <p>Using the application key, you can become an admin user.</p>
+            <div className="globals__siblings">
+                <div className="globals__input-wrapper">
+                    <label htmlFor="settings__application-key">Application key</label>
+                    <input
+                        value={applicationKey}
+                        onKeyDown={handleKeypressForAdminify}
+                        onChange={(e) => setApplicationKey(e.target.value)}
+                        id="settings__application-key"
+                        type="text" />
+                </div>
+                <div className="globals__input-wrapper"></div>
+            </div>
+            <p>
+                <button
+                    onClick={() => adminifyUser(application.user_id, applicationKey, application.action_token)}
+                    className="button-anchor">
+                    Confirm application key and become an admin
+                </button>
+            </p>
+        </div>
+    )
+
+    const processedRequestMessage = (
+        <p>You should receive an e-mail message in the next few minutes.</p>
+    )
+
+    const requestPasswordResetMessage = resetPasswordRequested ? (processedRequestMessage) : (
+        <p>
+            <button
+                onClick={() => {
+                    requestResetPassword(user.username)
+                    setResetPasswordRequested(true)
+                }}
+                className="button-anchor">
+                Request link to update password
+            </button>
+        </p>
+    )
+
+    const requestSecretCodesResetMessage = resetSecretCodesRequested ? (processedRequestMessage) : (
+        <p>
+            <button
+                onClick={() => {
+                    requestResetSecretCodes(user.username)
+                    setResetSecretCodesRequested(true)
+                }}
+                className="button-anchor">
+                Proceed to recreate recovery code and secret code generator
+            </button>
+        </p>
+    )
+
+    if (loading.includes('user') || user === undefined || application === undefined) {
+        return (
+            <>
+                <h2>Password &amp; Security</h2>
+                <div className="security-root"></div>
+            </>
+        )
+    }
+
     return (
         <>
             <h2>Password &amp; Security</h2>
@@ -13,7 +103,7 @@ const security = () => {
                         Update your password through a magic link sent to your e-mail account. It will generate
                         a temporary token so you can securily modify your account password.
                     </p>
-                    <p><button className="button-anchor">Request link to update password</button></p>
+                    {requestPasswordResetMessage}
                 </div>
                 <div className="globals__warning-box">
                     <h3>Recreate recovery code and secret code generator</h3>
@@ -21,23 +111,32 @@ const security = () => {
                         Through this request, you will recreate your account recovery code and the secret code generator
                         used in your secondary factor authenticator app. This process is irreversible.
                     </p>
-                    <p><button className="button-anchor">Proceed to recreate recovery code and secret code generator</button></p>
+                    {requestSecretCodesResetMessage}
                 </div>
-                <div className="globals__warning-box">
-                    <h3>Become an admin user</h3>
-                    <p>Using the application key, you can become an admin user.</p>
-                    <div className="globals__siblings">
-                        <div className="globals__input-wrapper">
-                            <label htmlFor="settings__application-key">Application key</label>
-                            <input id="settings__application-key" value="" type="text" />
-                        </div>
-                        <div className="globals__input-wrapper"></div>
-                    </div>
-                    <p><button className="button-anchor">Confirm application key and become an admin</button></p>
-                </div>
+                {adminifyUserBox}
             </div>
         </>
     )
 }
 
-export default security
+const mapStateToProps = state => {
+    return {
+        loading: state.root.loading,
+        application: state.root.application,
+        user: state.root.user
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchUserProfile: (id, token) => dispatch(actions.fetchUserProfile(id, token)),
+        requestResetPassword: (username) => dispatch(actions.requestResetPassword(username)),
+        requestResetSecretCodes: (username) => dispatch(actions.requestResetSecretCodes(username)),
+        adminifyUser: (id, key, token) => dispatch(actions.adminifyUser(id, key, token))
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(security)
