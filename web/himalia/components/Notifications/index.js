580bd6b29c0d985b143b45c9355cdbc224ed1561
Carlos A <588595+earaujoassis@general.noreply.quatrolabs.com>
2023-09-03 22:17:56 -0300

    feature: create himalia/notifications views

---
+++ web/himalia/components/Notifications/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './style.css'
+
+const notifications = () => {
+    return (
+        <>
+            <h2>Notifications</h2>
+            <div className="notifications-root">
+                <p>Notifications settings are not available yet.</p>
+            </div>
+        </>
+    )
+}
+
+export default notifications
