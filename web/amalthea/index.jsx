739a477a54f31e5ba4777785c82dab132ae395cf
Carlos A. <588595+earaujoassis@general.noreply.quatrolabs.com>
2025-06-04 22:13:47 -0300

    refactor: remove flux definitions from amalthea

--- web/amalthea/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import OwnerUpdate from './components/OwnerUpdate.jsx'
-
-ReactDOM.render(<OwnerUpdate />, document.getElementById('application-context'))
