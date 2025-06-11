a55289c2ffb412fff782b4781116adc03c5bf4e8
Carlos A. <588595+earaujoassis@general.noreply.quatrolabs.com>
2025-06-11 13:47:36 -0300

    feature: add tests for unsupported grants

--- test/helpers/oauth_client.go
+++ test/helpers/oauth_client.go
@@ -39,52 +39,6 @@ func NewOAuthTestClient(baseURL string) *OAuthTestClient {
 	}
 }
 
-func (c *OAuthTestClient) LoginUser(holder, password, passcode string) *TestResponse {
-	requestUrl := c.baseURL + "/api/sessions/create"
-	formData := url.Values{}
-	formData.Set("holder", holder)
-	formData.Set("password", password)
-	formData.Set("passcode", passcode)
-	encoded := formData.Encode()
-	request, _ := http.NewRequest("POST", requestUrl, strings.NewReader(encoded))
-	request.Header.Set("X-Requested-By", "SpaceApi")
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	response, err := c.httpClient.Do(request)
-	return parseResponse(response, err)
-}
-
-func (c *OAuthTestClient) GetAuthorization(responseType, clientID, redirectURI, state string) *TestResponse {
-	params := url.Values{}
-	params.Set("response_type", responseType)
-	params.Set("client_id", clientID)
-	params.Set("redirect_uri", redirectURI)
-	params.Set("state", state)
-
-	authURL := fmt.Sprintf("%s/oauth/authorize?%s", c.baseURL, params.Encode())
-	resp, err := c.httpClient.Get(authURL)
-	return parseResponse(resp, err)
-}
-
-func (c *OAuthTestClient) PostAuthorization(responseType, clientID, redirectURI, state string, authorize bool) *TestResponse {
-	params := url.Values{}
-	params.Set("response_type", responseType)
-	params.Set("client_id", clientID)
-	params.Set("redirect_uri", redirectURI)
-	params.Set("state", state)
-
-	authURL := fmt.Sprintf("%s/oauth/authorize?%s", c.baseURL, params.Encode())
-	if authorize {
-		resp, err := c.httpClient.Post(authURL, "application/x-www-form-urlencoded", strings.NewReader(""))
-		return parseResponse(resp, err)
-	} else {
-		formData := url.Values{}
-		formData.Set("access_denied", "true")
-		encoded := formData.Encode()
-		resp, err := c.httpClient.Post(authURL, "application/x-www-form-urlencoded", strings.NewReader(encoded))
-		return parseResponse(resp, err)
-	}
-}
-
 func (c *OAuthTestClient) StartSession(user *factory.User) {
 	code, _ := totp.GenerateCode(user.CodeSecretKey, time.Now())
 	response := c.LoginUser(user.Username, user.Passphrase, code)
@@ -112,4 +66,62 @@ func (c *OAuthTestClient) HasSessionCookie() bool {
 	}
 
 	return false
+}
+
+func (c *OAuthTestClient) LoginUser(holder, password, passcode string) *TestResponse {
+	requestUrl := c.baseURL + "/api/sessions/create"
+	formData := url.Values{}
+	formData.Set("holder", holder)
+	formData.Set("password", password)
+	formData.Set("passcode", passcode)
+	encoded := formData.Encode()
+	request, _ := http.NewRequest("POST", requestUrl, strings.NewReader(encoded))
+	request.Header.Set("X-Requested-By", "SpaceApi")
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	response, err := c.httpClient.Do(request)
+	return parseResponse(response, err)
+}
+
+func (c *OAuthTestClient) GetAuthorize(responseType, clientID, redirectURI, state string) *TestResponse {
+	params := url.Values{}
+	params.Set("response_type", responseType)
+	params.Set("client_id", clientID)
+	params.Set("redirect_uri", redirectURI)
+	params.Set("state", state)
+
+	authURL := fmt.Sprintf("%s/oauth/authorize?%s", c.baseURL, params.Encode())
+	resp, err := c.httpClient.Get(authURL)
+	return parseResponse(resp, err)
+}
+
+func (c *OAuthTestClient) PostAuthorize(responseType, clientID, redirectURI, state string, authorize bool) *TestResponse {
+	params := url.Values{}
+	params.Set("response_type", responseType)
+	params.Set("client_id", clientID)
+	params.Set("redirect_uri", redirectURI)
+	params.Set("state", state)
+
+	authURL := fmt.Sprintf("%s/oauth/authorize?%s", c.baseURL, params.Encode())
+	if authorize {
+		resp, err := c.httpClient.Post(authURL, "application/x-www-form-urlencoded", strings.NewReader(""))
+		return parseResponse(resp, err)
+	} else {
+		formData := url.Values{}
+		formData.Set("access_denied", "true")
+		encoded := formData.Encode()
+		resp, err := c.httpClient.Post(authURL, "application/x-www-form-urlencoded", strings.NewReader(encoded))
+		return parseResponse(resp, err)
+	}
+}
+
+func (c *OAuthTestClient) PostToken(clientBasicAuth, grantType string) *TestResponse {
+	formData := url.Values{}
+	formData.Set("grant_type", grantType)
+	encoded := formData.Encode()
+	requestUrl := fmt.Sprintf("%s/oauth/token", c.baseURL)
+	request, _ := http.NewRequest("POST", requestUrl, strings.NewReader(encoded))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Set("Authorization", fmt.Sprintf("Basic %s", clientBasicAuth))
+	response, err := c.httpClient.Do(request)
+	return parseResponse(response, err)
 }
