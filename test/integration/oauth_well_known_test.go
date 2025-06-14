b97fd35a5ad59d739544eb61b8468c6f259fc0ad
Carlos A. <588595+earaujoassis@general.noreply.quatrolabs.com>
2025-06-14 12:22:48 -0300

    chore: push coverage reports to Codecov

--- test/integration/oauth_well_known_test.go
+++ test/integration/oauth_well_known_test.go
@@ -1,23 +1,19 @@
 package integration
 
-import (
-	"strings"
-)
-
 func (s *OAuthProviderSuite) TestWellKnownMetadata() {
 	s.Run("should return a valid well-known metadata", func() {
 		response := s.Client.GetMetadata()
 		json := response.JSON
 
 		s.Equal(200, response.StatusCode)
-		s.Equal("public read", strings.Join(interfaceSliceToStringSlice(json["scopes_supported"].([]interface{})), " "))
-		s.Equal("code", strings.Join(interfaceSliceToStringSlice(json["response_types_supported"].([]interface{})), " "))
-		s.Equal("query", strings.Join(interfaceSliceToStringSlice(json["response_modes_supported"].([]interface{})), " "))
-		s.Equal("authorization_code", strings.Join(interfaceSliceToStringSlice(json["grant_types_supported"].([]interface{})), " "))
-		s.Equal("client_secret_basic", strings.Join(interfaceSliceToStringSlice(json["token_endpoint_auth_methods_supported"].([]interface{})), " "))
+		s.Equal("public read", jsonValueAsSingleString(json["scopes_supported"]))
+		s.Equal("code", jsonValueAsSingleString(json["response_types_supported"]))
+		s.Equal("query", jsonValueAsSingleString(json["response_modes_supported"]))
+		s.Equal("authorization_code", jsonValueAsSingleString(json["grant_types_supported"]))
+		s.Equal("client_secret_basic", jsonValueAsSingleString(json["token_endpoint_auth_methods_supported"]))
 		s.Equal("https://github.com/earaujoassis/space", json["service_documentation"])
-		s.Equal("en-US", strings.Join(interfaceSliceToStringSlice(json["ui_locales_supported"].([]interface{})), " "))
-		s.Equal("client_secret_basic", strings.Join(interfaceSliceToStringSlice(json["revocation_endpoint_auth_methods_supported"].([]interface{})), " "))
-		s.Equal("client_secret_basic", strings.Join(interfaceSliceToStringSlice(json["introspection_endpoint_auth_methods_supported"].([]interface{})), " "))
+		s.Equal("en-US", jsonValueAsSingleString(json["ui_locales_supported"]))
+		s.Equal("client_secret_basic", jsonValueAsSingleString(json["revocation_endpoint_auth_methods_supported"]))
+		s.Equal("client_secret_basic", jsonValueAsSingleString(json["introspection_endpoint_auth_methods_supported"]))
 	})
 }
