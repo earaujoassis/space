3f2682804748adb05ac5d3b8925196303b416eb5
Carlos A. <588595+earaujoassis@general.noreply.quatrolabs.com>
2025-06-17 00:11:20 -0300

    chore: project cleanup and upgrades

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,8 +8,6 @@ npm install -g yarn
 npm install -g allure-commandline
 yarn install && yarn build
 
-go install honnef.co/go/tools/cmd/staticcheck@latest
-go install gotest.tools/gotestsum@latest
 go install github.com/go-task/task/v3/cmd/task@latest
 task setup
 task setup:jwks
